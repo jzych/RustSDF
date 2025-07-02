97c2da1fd022cb6c7697c9991a43e9a9a21ad17d
jzych <46497556+jzych@users.noreply.github.com>
2025-07-02 11:04:27 +0200

    Test test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
     println!("Hello RustSDF!");
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+    }
+}
